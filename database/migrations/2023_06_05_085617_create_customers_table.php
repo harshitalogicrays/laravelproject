b04415038edbd9760cb65fc327ebd251d8fcbb24
harshita <harshita@logicrays.com>
2023-06-05 15:48:52 +0530

    ok1

---
+++ database/migrations/2023_06_05_085617_create_customers_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id('customer_id');
+            $table->string('name',60);
+            $table->string('email',50)->unique();
+            $table->integer('mobile')->nullable();
+            $table->text('address');
+            $table->string('password');
+            $table->enum('gender',['M','F','O']);
+            $table->date('dob')->nullable();
+            $table->boolean('isActive')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+};
--- database/migrations/2023_06_05_092458_create_products_table.php
+++ database/migrations/2023_06_05_092458_create_products_table.php
@@ -13,15 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('firstname');
-            $table->string('lastname');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps(); // created_at and updated_at
+            $table->timestamps();
         });
     }
 
@@ -32,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
 };
